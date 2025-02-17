472c415e47f8ffc33b78ed79c3b5f40d5deb525b
Bikash Santra <89884891+BikashSantra03@users.noreply.github.com>
2025-02-17 19:31:44 +0530

    Add function addedd

--- index.c
+++ index.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+int add()
+{
+    int a = 5;
+    int b = 10;
+    return a + b;
+}
 int main()
 {
+    int sum = add();
+    printf("The sum is: %d\n", sum);
     return 0;
 }
