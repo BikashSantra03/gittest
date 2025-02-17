179f123218d4afb21d777b15884a4bcca29d569e
Bikash Santra <89884891+BikashSantra03@users.noreply.github.com>
2025-02-17 19:38:01 +0530

    This is main

--- index.c
+++ index.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 
-int add()
-{
-    int a = 5;
-    int b = 10;
-    return a + b;
-}
+
 int main()
 {
-    int sum = add();
-    printf("The sum is: %d\n", sum);
-    return 0;
+        return 0;
 }
